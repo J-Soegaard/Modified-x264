da06c25d967d45280292fee43d7430ee9ebabdfd
J-Soegaard <jsog@fotonik.dtu.dk>
2016-05-23 16:10:47 +0200

    Modified MV candidate sets

--- common/mvpred_new.c
+++ common/mvpred_new.c
@@ -9,32 +9,13 @@ void x264_mb_predict_mv_ref16x16_EPZS( x264_t *h, int i_list, int i_ref, int16_t
     int16_t (*mvr)[2] = h->mb.mvr[i_list][i_ref];
     int i = 0, b = 0; // Counter for set B and C
 
-    #define SET_MVP(mvp) \
-    { \
-        CP32( mvc[i], mvp ); \
-        i++; \
-    }
-
-    #define SET_IMVP(xy) if( xy >= 0 ) \
-    { \
-        int shift = 1 + MB_INTERLACED - h->mb.field[xy]; \
-        int16_t *mvp = h->mb.mvr[i_list][i_ref<<1>>shift][xy]; \
-        mvc[i][0] = mvp[0]; \
-        mvc[i][1] = mvp[1]<<1>>shift; \
-        i++; \
-    }
-
+    
+    /* No (0,0) MV */ 
+    /* mvc[i][0] = 0;
+    mvc[i][1] = 0; // (0,0) MV
+    i++; */ 
 
     /* EPZS Set B */
-    mvc[i][0] = 0;
-    mvc[i][1] = 0; // (0,0) MV
-    i++;
-    SET_MVP( mvr[h->mb.i_mb_left_xy[0]] );  // Spatial left
-    SET_MVP( mvr[h->mb.i_mb_top_xy] );      // Spatial top
-    SET_MVP( mvr[h->mb.i_mb_topright_xy] ); // Spatial top-right
-    b += 4;
-    #undef SET_IMVP
-    #undef SET_MVP
     if( h->fref[0][0]->i_ref[0] > 0 )
     {
         x264_frame_t *l0 = h->fref[0][0];
@@ -51,7 +32,7 @@ void x264_mb_predict_mv_ref16x16_EPZS( x264_t *h, int i_list, int i_ref, int16_t
             mvc[i][1] = (l0->mv16x16[mb_index][1]*scale + 128) >> 8; \
             i++; \
         }
-        SET_TMVP(0,0);                          // Co-located at t-1 (Last of set B)
+        SET_TMVP(0,0);                          // Co-located at t-1 (Only part of set B)
         b++;
 
         /* EPZS Set C */ // Without "accelerated" MV predictor
@@ -66,6 +47,27 @@ void x264_mb_predict_mv_ref16x16_EPZS( x264_t *h, int i_list, int i_ref, int16_t
 
         #undef SET_TMVP        
     }   
+
+    #define SET_MVP(mvp) \
+    { \
+        CP32( mvc[i], mvp ); \
+        i++; \
+    }
+
+    #define SET_IMVP(xy) if( xy >= 0 ) \
+    { \
+        int shift = 1 + MB_INTERLACED - h->mb.field[xy]; \
+        int16_t *mvp = h->mb.mvr[i_list][i_ref<<1>>shift][xy]; \
+        mvc[i][0] = mvp[0]; \
+        mvc[i][1] = mvp[1]<<1>>shift; \
+        i++; \
+    }
+    SET_MVP( mvr[h->mb.i_mb_left_xy[0]] );  // Spatial left
+    SET_MVP( mvr[h->mb.i_mb_top_xy] );      // Spatial top
+    SET_MVP( mvr[h->mb.i_mb_topright_xy] ); // Spatial top-right
+
+    #undef SET_IMVP
+    #undef SET_MVP
 
     *i_mvc = i;
     *b_mvc = b;
